02ca45968f41c04962456544b33b391dd728bb44
AnonC <104805387+Ac-plus@users.noreply.github.com>
2026-03-08 21:46:51 +0800

    Add files via upload

---
+++ Week2/gbn_sample.sh
@@ -0,0 +1,6 @@
+#./progt <msg_num> <loss> <corruption> <snd_time> <trace> <runi>
+# 为控制变量，每次都发送20条massage
+./gbn 20 0.0 0.0 100 2 1
+./gbn 20 0.0 0.5 100 2 2
+./gbn 20 0.5 0.0 100 2 3
+./gbn 20 0.5 0.5 100 2 4
