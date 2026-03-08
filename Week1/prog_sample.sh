3c43195e8539333ee2666ec4c89a14b4a1ff7a77
AnonC <104805387+Ac-plus@users.noreply.github.com>
2026-03-08 21:45:18 +0800

    Add files via upload

---
+++ Week1/prog_sample.sh
@@ -0,0 +1,6 @@
+#./progt <msg_num> <loss> <corruption> <snd_time> <trace> <runi>
+# 为控制变量，每次都发送20条massage
+./prog.o 20 0.0 0.0 1000 2 1
+./prog.o 20 0.0 0.5 1000 2 2
+./prog.o 20 0.5 0.0 1000 2 3
+./prog.o 20 0.5 0.5 1000 2 4
